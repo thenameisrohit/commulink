ac35eaf212765db2dac80d5850527b37cec2a724
thenameisrohit <127139000+thenameisrohit@users.noreply.github.com>
2024-03-29 18:35:23 +0530

    Update route.ts

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -69,7 +69,7 @@ export async function GET(
     let nextCursor = null;
 
     if (messages.length === MESSAGES_BATCH) {
-      nextCursor = messages.length > 0 ? messages[MESSAGES_BATCH - 1].id : null;
+      nextCursor = messages[MESSAGES_BATCH - 1].id;
     }
 
     return NextResponse.json({
